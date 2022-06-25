d2b9e1416c3450242dabf3bd8131dd343a1af3b5
Karan <karanmangtani5@gmail.com>
2022-06-25 12:05:51 +0530

    email changes

--- controllers/contactUs.js
+++ controllers/contactUs.js
@@ -20,7 +20,7 @@ const sendEmail = (params) => {
   const mailData = {
     from: 'Info@padelleagueuae.com',  // sender address
     to: params.email,   // list of receivers
-    subject: 'PaddlePro connect',
+    subject: 'Padel League connect',
     text: 'Email From PaddlePro',
     html: `Hey <b> ${params.firstName } ${params.lastName || ''}</b><br/>
             <p>Thank you for contacting Padel League UAE, Our team will be in touch very soon</p>
@@ -34,13 +34,12 @@ const sendEmailToCompany = (params) => {
   const mailData = {
     from: 'Info@padelleagueuae.com',  // sender address
     to: "Info@padelleagueuae.com",   // list of receivers
-    subject: 'PaddlePro: Contact Us',
+    subject: 'Padel League - Contact Us',
     text: 'Email From PaddlePro',
     html: ` <b> Contact Details</b><br/><br/>
-            Name: ${params.firstName} ${params.lastName}<br/>
+            Name: ${params.firstName} ${params.lastName || ''}<br/>
             Email: ${params.email}<br/>
             Phone: ${params.phoneNo}<br/>
-            country: ${params.country}<br/>
 `,
   };
   sendMail(mailData);
