8acb24f65ab34f44ebf92cb80829b643f70b5d7e
khushaldusseja <khushal.dusseja@gmail.com>
2022-06-26 15:33:52 +0400

    Update registrations.js
    email subject update

--- controllers/registrations.js
+++ controllers/registrations.js
@@ -48,7 +48,7 @@ const sendEmailToCompany = (players) => {
   const mailData = {
     from: 'Info@padelleagueuae.com',  // sender address
     to: "Info@padelleagueuae.com",   // list of receivers
-    subject: 'Padel League: New Regitration',
+    subject: 'Padel League: Regitration',
     text: 'Email From Padel League',
     html: ` <b> New Team Registered ${players[0].teamName}</b><br/><br/>
     ${teamHtml}`
